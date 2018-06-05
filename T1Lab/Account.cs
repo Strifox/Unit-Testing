fb2c25a5c21b5979e08efadabfdddbeae03664f7
Johan Svensson <johan.svensson93@outlook.com>
2018-06-05 15:43:48 +0200

    Initial Commit
    Final commit

---
+++ T1Lab/Account.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace T1Lab
+{
+    public class Account
+    {
+        public double Balance { get; private set; }
+        public double Interest { get; set; }
+
+        public Account(double initialBalance, double interest)
+        {
+            //if (initialBalance < 1)
+            //    throw new Exception("Value is to low, must be 1 dollar or higher"); ;
+            //if (double.IsNaN(initialBalance))
+            //    throw new Exception("Must enter a number");
+            //if (double.IsInfinity(initialBalance))
+            //    throw new Exception("You have enter a to low or to high number");
+
+            //if (interest < double.Epsilon)
+            //    throw new Exception("Value is to low, must be higher"); ;
+            //if (double.IsNaN(interest))
+            //    throw new Exception("Must enter a number");
+            //if (double.IsInfinity(interest))
+            //    throw new Exception("You have enter a to low or to high number");
+
+            Balance = initialBalance;
+            Interest = interest;
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount < 10)
+                throw new Exception("Value is to low, must be 10 dollar or higher");
+            if (double.IsNaN(amount))
+                throw new Exception("Must enter a number");
+            if (double.IsInfinity(amount))
+                throw new Exception("You have enter a to low or to high number");
+
+            Balance += amount;
+            Console.WriteLine($"Successfully deposited {amount} dollar.\nYou have {Balance} dollar in your account.");
+
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount < 10)
+                throw new Exception("Value is to low, lowest amount to withdraw is 10 dollar");
+            if (Balance < 10)
+                throw new Exception("Not enough funds in your account for a withdrawal");
+            if (double.IsNaN(amount))
+                throw new Exception("Must enter a number");
+            if (double.IsInfinity(amount))
+                throw new Exception("You have enter a to low or to high number");
+
+
+            Balance -= amount;
+            Console.WriteLine($"Successfully withdrawed {amount} dollar.\nYou have {Balance} dollar in your account.");
+        }
+
+        public bool Transfer(Account targetedAccount, double amount)
+        {
+            if (targetedAccount == null)
+                throw new Exception("Target account is null, cannot access it.\n Null is not allowed");
+            if (amount < 5)
+                throw new Exception($"You cannot transfer less than 5 dollar, your current amount is {Balance}");
+            if (Balance < 5)
+                throw new Exception($"Not enough funds in your account for a transfer, your current amount is {Balance}");
+            if (this == targetedAccount)
+                throw new Exception($"You cannot transfer to the same account");
+            if (double.IsNaN(amount))
+                throw new Exception("Must enter a number");
+            if (double.IsInfinity(amount))
+                throw new Exception("You have enter a to low or to high number");
+
+            targetedAccount.Balance += amount;
+            Balance -= amount;
+
+            Console.WriteLine($"You transfered {amount} dollar from your account, you have {Balance} dollar in your account");
+
+            return true;
+        }
+
+        public double CalculateInterest()
+        {
+            if(double.IsNaN(Interest))
+                throw new Exception("Must enter a number");
+            if (double.IsNaN(Balance))
+                throw new Exception("Must enter a number");
+            if (double.IsInfinity(Interest))                                                                                                                    
+                throw new Exception("You have enter a to low or to high number");
+            if (double.IsInfinity(Balance))
+                throw new Exception("Balance is either to low or to high number");
+
+            double rate = Interest * Balance;
+            double newBalance = rate + Balance;
+            Balance = newBalance;
+
+            Console.WriteLine($"Your current interest is {newBalance}");
+
+            return rate;
+        }
+
+    }
+}
---
+++ XUnitTestT1Lab/UnitTest1.cs
@@ -0,0 +1,217 @@
+using System;
+using Xunit;
+using T1Lab;
+
+namespace XUnitTestT1Lab
+{
+    public class UnitTest1
+    {
+        public Account CreateAccount(double balance, double interest )
+        {
+            return new Account(balance, interest);
+        }
+
+
+        #region Deposit Tests
+
+        [Theory]
+        [InlineData(double.NegativeInfinity)]
+        [InlineData(double.PositiveInfinity)]
+        [InlineData(double.Epsilon)]
+        [InlineData(3)]
+        [InlineData(double.NaN)]
+        public void Deposit_IfAmountIsToLow_ExceptionThrown(double amount)
+        {
+            Account account = CreateAccount(10000, 0.02);
+
+            Assert.Throws<Exception>(() =>
+            {
+                account.Deposit(amount);
+            });
+        }
+
+
+        [Fact]
+        public void Deposit_IncreaseBalance_Successful()
+        {
+            Account account = CreateAccount(10000, 0.02);
+
+            double deposit = 1000;
+            account.Deposit(deposit);
+
+            double expectedBalance = 11000;
+            double actualBalance = account.Balance;
+
+            Assert.Equal(expectedBalance, actualBalance);
+
+        }
+
+        #endregion
+
+        #region Withdrawal Tests
+
+
+        [Theory]
+        [InlineData(double.NegativeInfinity)]
+        [InlineData(double.PositiveInfinity)]
+        [InlineData(double.Epsilon)]
+        [InlineData(5)]
+        [InlineData(double.NaN)]
+        public void WithDraw_IfAmountIsToLow_ExceptionThrown(double amount)
+        {
+            Account account = CreateAccount(10000, 0.02);
+
+            Assert.Throws<Exception>(() =>
+            {
+                account.Withdraw(amount);
+            });
+          
+        }
+
+        [Theory]
+        [InlineData(double.NegativeInfinity)]
+        [InlineData(double.PositiveInfinity)]
+        [InlineData(double.Epsilon)]
+        [InlineData(5)]
+        public void WithDraw_IfBalanceIsToLow_ExceptionThrown(double amount)
+        {
+            Account account = CreateAccount(3, 0.02);
+
+            Assert.Throws<Exception>(() =>
+            {
+                account.Withdraw(amount);
+            });
+        }
+     
+        [Fact]
+        public void WithDraw_FromBalance_Successful()
+        {
+            Account account = CreateAccount(10000, 0.02);
+            double withdrawal = 1000;
+            account.Withdraw(withdrawal);
+            double expectedBalance = 9000;
+            double actualBalance = account.Balance;
+
+            Assert.Equal(expectedBalance, actualBalance);
+        }
+        #endregion
+
+        #region Transfer Tests
+
+        [Theory]
+        [InlineData(double.Epsilon)]
+        [InlineData(double.NaN)]
+        [InlineData(double.NegativeInfinity)]
+        [InlineData(double.PositiveInfinity)]
+        public void Transfer_IfAmountIsToLow_ExceptionThrown(double amount)
+        {
+            Account account = new Account(1000, 0.02);
+            Account newAccount = new Account(400, 0.02);
+
+            Assert.Throws<Exception>(() =>
+            {
+                account.Transfer(newAccount, amount);
+            });
+        }
+
+        [Fact]
+        public void Transfer_InvalidAccount_ExceptionThrown()
+        {
+            Account account = new Account(1000, 0.02);
+
+            double amount = 100;
+            Assert.Throws<Exception>(() =>
+            {
+                account.Transfer(account, amount);
+            });
+        }
+
+        [Fact]
+        public void Transfer_IfBalanceIsToLow_ExceptionThrown()
+        {
+            Account account = new Account(3, 0.02);
+            Account newAccount = new Account(400, 0.02);
+
+            double amount = 100;
+
+            Assert.Throws<Exception>(() =>
+            {
+                account.Transfer(newAccount, amount);
+            });
+        }
+
+        [Fact]
+        public void Transfer_ToAnotherAccount_Successful()
+        {
+            Account account = new Account(10000, 0.02);
+            Account newAccount = new Account(400, 0.02);
+
+            double amountToTransfer = 4000;
+            account.Transfer(newAccount, amountToTransfer);
+
+            double expectedAccountBalance = 4400;
+            double actualAccountBalance = newAccount.Balance;
+
+            Assert.Equal(expectedAccountBalance, actualAccountBalance);
+
+        }
+        #endregion
+
+        #region Interest Tests
+
+        [Theory]
+        [InlineData(double.NegativeInfinity)]
+        [InlineData(double.PositiveInfinity)]
+        [InlineData(double.NaN)]
+        public void Calculate_IfInvalidBalance_ExceptionThrown(double amount)
+        {
+            Account account = CreateAccount(amount, 0.02);
+
+            Assert.Throws<Exception>(() =>
+            {
+                account.CalculateInterest();
+            });
+        }
+
+        [Theory]
+        [InlineData(double.NegativeInfinity)]
+        [InlineData(double.PositiveInfinity)]
+        [InlineData(double.NaN)]
+        public void Calculate_IfInvalidInterest_ExceptionThrown(double amount)
+        {
+            Account account = CreateAccount(1000, amount);
+
+            Assert.Throws<Exception>(() =>
+            {
+                account.CalculateInterest();
+            });
+        }
+
+        [Fact]
+        public void CalculateInterest_Successful()
+        {
+            Account account = CreateAccount(1000, 0.02);
+
+
+            double actualrate = account.CalculateInterest();
+            double expectedRate = 20;
+
+            Assert.Equal(expectedRate, actualrate);
+        } 
+
+        [Fact]
+        public void CalculateBalanceWithInterest_Successful()
+        {
+            Account account = CreateAccount(1000, 0.02);
+            account.CalculateInterest();
+
+            double actualBalance= account.Balance;
+            double expectedBalance = 1020;
+
+            Assert.Equal(expectedBalance, actualBalance);
+        }
+        #endregion
+
+
+    }
+}
