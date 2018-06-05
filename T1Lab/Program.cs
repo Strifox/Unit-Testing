771043a281a71eaccc169e8f556cee28fe88e58f
Johan Svensson <johan.svensson93@outlook.com>
2018-06-05 10:34:51 +0200

    Add project files.

---
+++ T1Lab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace T1Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
